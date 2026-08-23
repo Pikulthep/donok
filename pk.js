d9ddfa4e725665ad01639e1dc3d6ad3c65fdf3df
Sasima <steconx01@gmail.com>
2026-08-23 09:05:52 +0700

    Create pk.js

---
+++ pk.js
@@ -0,0 +1,70 @@
+{
+  "name": "🗺️ สารคดี",
+  "author": "My Playlist",
+  "image": "",
+  "stations": [
+    {
+      "name": "TLC",
+      "url": "https://doodii-1.akamomo558.top/xstream/tlc011/playlist.m3u8",
+      "image": "https://doodii.me/images/tv/kWHZjQcrYq7xc6tWnfHFl6dSXwRMO7Utlc.jpg",
+      "referer": "https://doodii.me",
+      "playInNatPlayer": true
+    },
+    {
+      "name": "SCI",
+      "url": "https://doodii-1.akamomo558.top/xstream/sci01/playlist.m3u8",
+      "image": "https://doodii.me/images/tv/T8U9wGLVglgZRs4oGaMN6ktsV76ioIbEWhw8dLW1GnwgrxG76dAEPVKbufSlq320240418210428TrueExploreSci.jpg",
+      "referer": "https://doodii.me",
+      "playInNatPlayer": true
+    },
+    {
+      "name": "wild",
+      "url": "https://doodii-1.akamomo558.top/xstream/wild01/playlist.m3u8",
+      "image": "https://doodii.me/images/tv/Dc98XfsJVlmol48wpuFrP07YxtUk9XgrcqI50Ir5cF4EAohnHe0COIGqSFEPdt20240418210353TrueExploreWild.jpg",
+      "referer": "https://doodii.me",
+      "playInNatPlayer": true
+    },
+    {
+      "name": "discovery",
+      "url": "https://doodii-1.akamomo558.top/xstream/discovery01/playlist.m3u8",
+      "image": "https://doodii.me/images/tv/i741kofRHNtQ1iOTw4tIocbV9oEy3gBRm6cTJ2qpQIl4h3Sbg2L0O3SchU1P8p20240418210226Discovery.jpg",
+      "referer": "https://doodii.me",
+      "playInNatPlayer": true
+    },
+    {
+      "name": "khongdee",
+      "url": "https://doodii-1.akamomo558.top/xstream/khongdee01/playlist.m3u8",
+      "image": "https://doodii.me/images/tv/w5fDOkiDKsG3X2gJY74MDeMoGMn1aLQของดี.png",
+      "referer": "https://doodii.me",
+      "playInNatPlayer": true
+    },
+    {
+      "name": "samrujlok",
+      "url": "https://doodii-1.akamomo558.top/xstream/khongdee01/chunks.m3u8",
+      "image": "https://doodii.me/images/tv/8UcNPKTwY2iRNYgsglTYOg6yFXuZivYhwlqMfGYykYbQxa2INjot4HL3974ezW20240418211443SamRujLok.jpg",
+      "referer": "https://doodii.me",
+      "playInNatPlayer": true
+    },
+    {
+      "name": "HISTORY",
+      "url": "https://doodii-1.akamomo558.top/xstream/history01/playlist.m3u8",
+      "image": "https://doodii.me/images/tv/PcNxQMYR8As9Q3KoVKlDu63kiKEYz2Ohistory.png",
+      "referer": "https://doodii.me",
+      "playInNatPlayer": true
+    },
+    {
+      "name": "mysci",
+      "url": "https://doodii-1.akamomo558.top/xstream/mysci01/playlist.m3u8",
+      "image": "https://doodii.me/images/tv/lPwRuozq3Gx6e0kS5wzkh8UWCctTw8iul9B2XYZScocq2lk1Lpd3lK1YE25bHh20240418211511MySci.jpg",
+      "referer": "https://doodii.me",
+      "playInNatPlayer": true
+    },
+    {
+      "name": "thainess",
+      "url": "https://doodii-1.akamomo558.top/xstream/mysci01/chunks.m3u8",
+      "image": "https://doodii.me/images/tv/oZjMkrFBfofFwwcXaZCr9jhnas9lE5T1JZYoEILB5rlyNG9g2Iqz7JFz3aKKqMthainess.png",
+      "referer": "https://doodii.me",
+      "playInNatPlayer": true
+    }
+  ]
+}
